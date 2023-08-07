e80d42efa4cef697cb5cbaddca9889d1a227b441
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-07 18:04:40 +0600

    :apple:updated

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Box  from '@mui/material/Box';
-import Button  from '@mui/material/Button';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Products = () => {
-    return (
-        <Box sx={{display:"flex", alignItems:"center", justifyContent:"center"}}> 
-        <Button sx={{my:"20px", bgcolor:"#264143"}} variant="contained">আরে মিয়া প্রোডাক্ট পরে অ্যাড করমু।তা কেমন হয়েছে, বস?
-
-  </Button>
-      </Box>
-    );
+  return (
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Button sx={{ my: "20px", bgcolor: "#264143",'&:hover': {
+       bgcolor: "red",
+    }, }} variant="contained">
+        আরে মিয়া প্রোডাক্ট পরে অ্যাড করমু।তা কেমন হয়েছে, বস?
+      </Button>
+    </Box>
+  );
 };
 
 export default Products;
