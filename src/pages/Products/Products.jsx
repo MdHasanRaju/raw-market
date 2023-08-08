b1c1722269ac50fda7a95adb1145d29f64912c89
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-08 11:38:17 +0600

    updated with color

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -7,9 +7,16 @@ const Products = () => {
     <Box
       sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
     >
-      <Button sx={{ my: "20px", bgcolor: "#264143",'&:hover': {
-       bgcolor: "red",
-    }, }} variant="contained">
+      <Button
+        sx={{
+          my: "20px",
+          bgcolor: "#264143",
+          "&:hover": {
+            bgcolor: "red",
+          },
+        }}
+        variant="contained"
+      >
         আরে মিয়া প্রোডাক্ট পরে অ্যাড করমু।তা কেমন হয়েছে, বস?
       </Button>
     </Box>
