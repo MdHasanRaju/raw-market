5b06fb95bdc6c6b8acea7ce59ba337b14b97feda
SShipon <nxshipon@gmail.com>
2023-08-09 13:36:48 +0600

    footer container add

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,15 +1,49 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import Typography  from '@mui/material/Typography';
+import Grid  from '@mui/material/Grid';
+import  Container  from '@mui/system/Container';
+import React from 'react';
 
-import { Container } from '@mui/material';
-
-
-
-export default function Footer() {
+const Footer = () => {
   return (
-    <Container>
-            <h1>git commmit</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, facere?</p>
-    </Container>
+    <div>
+        <Container>
+             <Grid container >
+                <Grid item  sx={{textAlign:'center'}} xs={12} sm={6} md={3}>
+                  <Typography variant='h2'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                </Grid>
+                <Grid item sx={{textAlign:'center'}} xs={12} sm={6} md={3}>
+                  <Typography variant='h2'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                </Grid>
+                <Grid item sx={{textAlign:'center'}} xs={12} sm={6} md={3}>
+                  <Typography variant='h2'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                </Grid>
+                <Grid item sx={{textAlign:'center'}} xs={12} sm={6} md={3}>
+                  <Typography variant='h2'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                  <Typography variant='h5'>Product</Typography>
+                </Grid>
+             </Grid>
+        </Container>
+    </div>
   );
-}
+};
+
+export default Footer;
