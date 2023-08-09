5ca3b3b993f1f47be0a7bf9dc18dd55bca7daeca
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-09 11:10:38 +0600

    :fire: about's text added again

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div>
             <Box component="p">
-                this is about page
+                this is about page..... Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ratione maiores recusandae, delectus explicabo suscipit, quam repellendus pariatur iusto magni qui ab corporis fugit aliquam quidem molestias impedit in eveniet?
             </Box>
         </div>
     );
