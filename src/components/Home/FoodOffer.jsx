fd929bef42ac5863e4ec733b605e788cffb806fe
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-12 13:48:51 +0600

    :rocket: offer section added

---
+++ src/components/Home/FoodOffer.jsx
@@ -0,0 +1,78 @@
+import { Box } from "@mui/system";
+import React from "react";
+import { Button, Grid, Typography } from "@mui/material";
+import offerImg from "../../assets/banners/offer-image.jpg";
+import videoBanner from "../../assets/banners/video-banner.jpg";
+import { Link } from "react-router-dom";
+
+const FoodOffer = () => {
+  return (
+    <Box>
+      <Grid container spacing={3}>
+        <Grid width="50%" item md={6}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <Box
+              sx={{ width: "100%" }}
+              component="img"
+              src={offerImg}
+              alt="offer food image"
+            />
+            <Box sx={{ position: "absolute", top: "30%", right: "8%" }}>
+              <Typography variant="body1">GET UP TO 30% OFF</Typography>
+              <Typography sx={{ mt: 1, mb: 3 }} variant="h5">
+                BRUSSELS foods
+              </Typography>
+              <Button
+                variant="outlined"
+                sx={{
+                  "&:hover": {
+                    outline: "none",
+                    bgcolor: "green",
+                    color: "white",
+                  },
+                }}
+                color="success"
+              >
+                Shop Now
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid width="50%" md={6}>
+            <Box sx={{position:"relative"}}>
+
+          <Box
+            sx={{ width: "100%" }}
+            component="img"
+            alt="video banner image"
+            src={videoBanner}
+          >
+            {/* <Link class="play-button popup-youtube" href="https://www.youtube.com/watch?v=0etCKCAsImI">
+                        <i class="fa-solid fa-play"></i>
+                        <span class="video-icon-sonar">
+                            <span class="video-icon-sonar-bfr"></span>
+                        </span>
+                    </Link> */}
+          </Box>
+          <Box sx={{bgcolor:"green", position:'absolute',border:"1px solid transparent", borderRadius:"50%", top:'40%', right:"41%",width:100, height:100, display:"flex", justifyContent:"center", alignItems:"center" }}>
+            <Link to="/https://www.youtube.com/watch?v=0etCKCAsImI">
+              <i style={{fontSize:"40px", color:"orange"}} class="fa-solid fa-play"></i>
+            </Link>
+          </Box>
+            </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default FoodOffer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ExquisiteService from "./ExquisiteService";
 import Banner from "./Banner";
+import FoodOffer from "./FoodOffer";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <FoodOffer/>
       <ExquisiteService />
     </>
   );
