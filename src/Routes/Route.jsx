2f9232b71f12311094a6294e70f1b003f686ec7a
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-13 10:32:16 +0600

    :banana: router updated

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -7,7 +7,7 @@ import NotFound from './../components/NotFound/NotFound';
 import Home from "../pages/Home/Home";
 import Products from './../pages/Products/Products';
 import About from './../pages/About/About';
-import Contact from "../pages/Contact/Contact";
+// import Contact from "./../pages/Contact/Contact";
 
 export const router = createBrowserRouter([
     {
@@ -31,10 +31,10 @@ export const router = createBrowserRouter([
             path:'/about',
             element:<About></About>
         },
-        {
-            path:'/contact',
-            element:<Contact></Contact>
-        },
+        // {
+        //     path:'/contact',
+        //     element:<Contact></Contact>
+        // },
       ]
     },
   ]);
