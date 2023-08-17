5f6f38826a8d4e64b97ba79948898f90971f31b5
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-17 22:08:54 +0600

    :rocket: not found page added

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,44 +1,38 @@
-import * as React from "react"; 
-import {
-  createBrowserRouter, 
-} from "react-router-dom"; 
+import * as React from "react";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import NotFound from './../components/NotFound/NotFound';
 import Home from "../pages/Home/Home";
-import Products from './../pages/Products/Products';
-import About from './../pages/About/About';
+import Products from "./../pages/Products/Products";
+import About from "./../pages/About/About";
 import Contact from "../pages/Contact/Contact";
-
-
+import NotFound from "../pages/NotFound/NotFound";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement:<NotFound></NotFound>,
-      children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        },
-        {
-            path:'/home',
-            element:<Home></Home>
-        },
-        {
-            path:'/products',
-            element:<Products></Products>
-        },
-        {
-            path:'/about',
-            element:<About></About>
-        },
-       {
-             path:'/contact',
-             element:<Contact></Contact>
-         }, 
-      ]
-    },
-  ]);
-
- 
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <NotFound></NotFound>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
