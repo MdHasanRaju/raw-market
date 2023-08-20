a4800e6ffb79eea5ad8a5ab3cb91854312e887ba
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-08-20 10:20:14 +0600

    :fire: timer updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import Box from "@mui/material/Box";
-import HomeComp from "./../../components/Home/Home";
+import Box from "@mui/material/Box"; 
+import Banner from "../../components/Home/Banner";
+import FoodOffer from "../../components/Home/FoodOffer";
+import ExquisiteService from "../../components/Home/ExquisiteService";
+import OfferTimer from "../../components/Home/OfferTimer";
 
 const Home = () => {
   return (
-    <Box>
-      <HomeComp />
+    <Box> 
+      <Banner />
+      <FoodOffer/>
+      <ExquisiteService />
+      <OfferTimer/>
     </Box>
   );
 };
