1544b0211c21eb378c48c498beec3dadc3ee8fba
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-09-11 11:43:06 +0600

    :rocket: fake data created

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import "./App.css";
 function App() {
   return (
     <Box>
-      https://i.ibb.co/QcF68w2/onion.webp https://i.ibb.co/gJXg8z6/ovacado.webp
-      https://i.ibb.co/jbFJm8L/pineapple.webp
-      https://i.ibb.co/Zck3Pyd/strawberry.webp
-      https://i.ibb.co/H72ftNj/tomato.jpg
-      https://i.ibb.co/7nx25pt/vegist-image-1.webp
-      https://i.ibb.co/zmsDFfg/vinegar.jpg
-      https://i.ibb.co/WpFQr1F/watarmelon.webp
-      https://i.ibb.co/10bYhrT/wiki.webp https://i.ibb.co/vxnCRKz/almond.webp
-      https://i.ibb.co/z6bTmYw/apple.webp https://i.ibb.co/7bCVSTV/banana.webp
-      https://i.ibb.co/K6h74pJ/bigcapsicum.jpg https://i.ibb.co/p281L2q/bin.jpg
-      https://i.ibb.co/ncRTcYp/brocoli.webp
-      https://i.ibb.co/dfYVfJB/cabbage.webp
-      https://i.ibb.co/HrW3mDf/carrot-jpgj.jpg
-      https://i.ibb.co/jJZ7fFb/coconut.webp https://i.ibb.co/fCrqNR4/graps.webp
-      https://i.ibb.co/hLKwW45/green-Capsicum.webp
-      https://i.ibb.co/2PtPzBc/lemon.webp https://i.ibb.co/zNRtwgq/mango.webp
+      "https://i.ibb.co/QcF68w2/onion.webp"
+      "https://i.ibb.co/gJXg8z6/ovacado.webp"
+      "https://i.ibb.co/jbFJm8L/pineapple.webp"
+      "https://i.ibb.co/Zck3Pyd/strawberry.webp"
+      "https://i.ibb.co/H72ftNj/tomato.jpg"
+      "https://i.ibb.co/7nx25pt/vegist-image-1.webp"
+      "https://i.ibb.co/zmsDFfg/vinegar.jpg"
+      "https://i.ibb.co/WpFQr1F/watarmelon.webp"
+      "https://i.ibb.co/10bYhrT/wiki.webp"
+      "https://i.ibb.co/vxnCRKz/almond.webp"
+      "https://i.ibb.co/z6bTmYw/apple.webp"
+      "https://i.ibb.co/7bCVSTV/banana.webp"
+      "https://i.ibb.co/K6h74pJ/bigcapsicum.jpg"
+      "https://i.ibb.co/p281L2q/bin.jpg" 
+      "https://i.ibb.co/ncRTcYp/brocoli.webp"
+      "https://i.ibb.co/dfYVfJB/cabbage.webp"
+      "https://i.ibb.co/HrW3mDf/carrot-jpgj.jpg"
+      "https://i.ibb.co/jJZ7fFb/coconut.webp"
+      "https://i.ibb.co/fCrqNR4/graps.webp"
+      "https://i.ibb.co/hLKwW45/green-Capsicum.webp"
+      "https://i.ibb.co/2PtPzBc/lemon.webp"
+      "https://i.ibb.co/zNRtwgq/mango.webp"
     </Box>
   );
 }
